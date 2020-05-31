488738968a8c7b9858a7e5d3ac7d200212848722
subramani simhachalam <iceapple@subramanis-iMac.local>
2020-05-31 15:25:36 +0530

    Initial commit with basic SSR setup (express server & client app)

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import renderer from './helpers/renderer';
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.send(renderer());
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
