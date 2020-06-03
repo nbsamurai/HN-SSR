6c6dd0fc0c55a2be8f8457591dbbb2a8940184f5
subramani simhachalam <iceapple@subramanis-iMac.local>
2020-06-03 19:25:42 +0530

    Added travis ci file

--- src/client/components/Page.js
+++ src/client/components/Page.js
@@ -4,32 +4,11 @@ import { fetchPage } from '../actions';
 import Graph from './Graph';
 import NewsFeed from './NewsFeed';
 
-// import {
-//   wrapper,
-//   isStriped,
-//   rtable,
-//   rtableRowHeader,
-//   rtableRow,
-//   rtableCell,
-//   columnHeading,
-//   commentCell,
-//   countCell,
-//   textCell,
-//   footerContainer,
-//   linkSeparator,
-//   linkButton,
-//   divider,
-//   subtext,
-//   hideButton,
-// } from '../styles';
-
 class Page extends Component {
   constructor(props) {
     super(props);
     this.state = { page: 0 };
     this.changePage = this.changePage.bind(this);
-    // this.getHostName = this.getHostName.bind(this);
-    // this.timeSince = this.timeSince.bind(this);
   }
 
   componentDidMount() {
@@ -60,70 +39,6 @@ class Page extends Component {
         <Graph ids={ids} votes={votes} />
       </>
     );
-
-    // return (
-    //   <>
-    //     <div style={wrapper}>
-    //       <div style={rtable}>
-    //         <div style={{ ...rtableRow, ...rtableRowHeader }}>
-    //           <div style={{ ...columnHeading, ...rtableCell, ...commentCell }}>
-    //             Comments
-    //           </div>
-    //           <div style={{ ...columnHeading, ...rtableCell, ...countCell }}>
-    //             Vote Count
-    //           </div>
-    //           <div style={{ ...columnHeading, ...rtableCell, ...countCell }}>
-    //             UpVote
-    //           </div>
-    //           <div style={{ ...columnHeading, ...rtableCell, ...textCell }}>
-    //             New Details
-    //           </div>
-    //         </div>
-    //         {this.props.page.hits.map((item, index) => (
-    //           <div
-    //             key={item.objectID}
-    //             style={index % 2 ? { ...rtableRow, ...isStriped } : rtableRow}
-    //           >
-    //             <div style={{ ...rtableCell, ...commentCell }}>
-    //               {item.num_comments}
-    //             </div>
-    //             <div style={{ ...rtableCell, ...countCell }}>{item.points}</div>
-    //             <div style={{ ...rtableCell, ...countCell }}>
-    //               {/* <button>vote</button> */}
-    //               <a style={{ cursor: 'pointer' }}>
-    //                 <div title='upvote' />
-    //               </a>
-    //             </div>
-    //             <div style={{ ...rtableCell, ...textCell }}>
-    //               {`${item.title} (${this.getHostName(item.url)}) by ${
-    //                 item.author
-    //               } ${this.timeSince(item.created_at_i)} ago [ `}
-    //               <a style={hideButton}>hide</a> {` ]`}
-    //             </div>
-    //           </div>
-    //         ))}
-    //       </div>
-    //       <div style={footerContainer}>
-    //         <div>
-    //           <button
-    //             style={linkButton}
-    //             onClick={() => this.changePage('prev')}
-    //           >
-    //             Previous
-    //           </button>{' '}
-    //           <span style={linkSeparator}>|</span>{' '}
-    //           <button
-    //             style={linkButton}
-    //             onClick={() => this.changePage('next')}
-    //           >
-    //             Next
-    //           </button>
-    //         </div>
-    //       </div>
-    //       <hr style={divider} />
-    //     </div>
-    //   </>
-    // );
   }
 }
 const mapStateToProps = ({ page }) => ({
